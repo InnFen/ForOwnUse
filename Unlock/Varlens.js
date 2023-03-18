367ead3fa5453bf87b5f81069430d38f17e069b9
InnFen <123163367+InnFen@users.noreply.github.com>
2023-03-18 23:09:29 +0800

    Update Varlens.js

--- Unlock/Varlens.js
+++ Unlock/Varlens.js
@@ -1,3 +1,17 @@
+/***********************************
+> 应用名称：Varlens
+> 脚本功能：解锁2045会员
+> 更新时间：2023-03-18
+> 特别说明：本脚本仅供学习交流使用
+            
+[rewrite_local]
+# ～ varlens解锁2045会员
+^https:\/\/mid\.zineapi\.com\/@varlens\/api\/user\/info\/ url script-response-body https://raw.githubusercontent.com/InnFen/forown/main/Unlock/Varlens.js
+
+[mitm]
+hostname = mid.zineapi.com
+***********************************/
+
 var InnFen = 
 {"success":true,"data":{"username":"InnFen","app_name":"InnFen","nick":null,"email":"","status":0,"preferences":null,"membership":{"role_name":"pro","begin_time":"2021-03-13T09:59:11.485Z","end_time":"2045-05-20T00:00:00.481Z","begin_time_ms":1615629551485,"end_time_ms":2378870040000},"social_apple_id":null}};
 
